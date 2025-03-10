a7e081b21ed6b39695248f66f49504892d0f6287
Breakmind4 <40310@prot.ac.th>
2025-03-10 15:31:00 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+number = int(input("Please enter your number"))
+if number < 0:
+    print("This number is Negative")
+elif number > 0:
+    print("This number is Positive")
+
+elif number == 0:
+    print("It's can't calculate because you put zero")
