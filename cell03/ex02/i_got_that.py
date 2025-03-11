402f0bd0e7add15a4a40e1f8d2c7acdc64251c66
Breakmind4 <40310@prot.ac.th>
2025-03-11 11:36:29 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+say = input("what your problem?")
+Stop = True
+while True:
+    Stop =  input("I got that don't worry")
+    if Stop =="Stop":
+        print("Don't")
+        break
+    else:
+        print("I got that")
